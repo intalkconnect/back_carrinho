1c5c4cf6eec83823487427493af96fe589138d05
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-01-17 20:36:26 -0300

    Update checkoutService.js

--- src/services/checkoutService.js
+++ src/services/checkoutService.js
@@ -56,7 +56,10 @@ const processCheckout = async (checkoutId, dataFim, dadosPessoais, enderecoEntre
 const getCheckouts = async () => {
     try {
         // Filtro para o status "confirmed"
-        const filtro = { status: 'confirmed' };
+        const filtro = {
+            status: "confirmed",
+            process: false,
+        };
 
         // Projeção dos campos que queremos retornar
         const camposProjecao = {
@@ -65,26 +68,76 @@ const getCheckouts = async () => {
             dataFim: 1,
             dataCompra: 1,
             orcamentoFinal: 1,
-            numero_orcamento: 1
+            numero_orcamento: 1,
         };
 
         // Busca no banco de dados com os filtros e campos projetados
         const checkouts = await Checkout.find(filtro, camposProjecao);
 
         if (!checkouts || checkouts.length === 0) {
-            throw new Error('Nenhum checkout encontrado');
+            throw new Error("Nenhum checkout encontrado");
         }
 
-        // Converte _id de ObjectId para string
-        checkouts.forEach((checkout) => {
-            checkout._id = checkout._id.toString();
-        });
+        // Formatação do retorno
+        const formattedCheckouts = checkouts.map((checkout) => ({
+            _id: checkout._id.toString(), // Converte ObjectId para string
+            dataCriacao: checkout.dataCriacao,
+            dataFim: checkout.dataFim,
+            status: checkout.status,
+            numero_orcamento: checkout.numero_orcamento,
+            dataCompra: checkout.dataCompra,
+            orcamentoFinal: {
+                checkout: checkout._id.toString(),
+                dataFim: checkout.orcamentoFinal?.dataFim,
+                dadosPessoais: {
+                    nomeCompleto: checkout.orcamentoFinal?.dadosPessoais?.nomeCompleto,
+                    cpf: checkout.orcamentoFinal?.dadosPessoais?.cpf,
+                    rg: checkout.orcamentoFinal?.dadosPessoais?.rg || "",
+                    celular: checkout.orcamentoFinal?.dadosPessoais?.celular,
+                    email: checkout.orcamentoFinal?.dadosPessoais?.email || "",
+                },
+                enderecoEntrega: {
+                    endereco: checkout.orcamentoFinal?.enderecoEntrega?.endereco,
+                    numero: checkout.orcamentoFinal?.enderecoEntrega?.numero,
+                    complemento: checkout.orcamentoFinal?.enderecoEntrega?.complemento,
+                    bairro: checkout.orcamentoFinal?.enderecoEntrega?.bairro,
+                    cidade: checkout.orcamentoFinal?.enderecoEntrega?.cidade,
+                    estado: checkout.orcamentoFinal?.enderecoEntrega?.estado,
+                    cep: checkout.orcamentoFinal?.enderecoEntrega?.cep,
+                    tipoFrete: checkout.orcamentoFinal?.enderecoEntrega?.tipoFrete,
+                },
+                localRetirada: checkout.orcamentoFinal?.localRetirada || null,
+                formaPagamento: checkout.orcamentoFinal?.formaPagamento,
+                produtos: checkout.orcamentoFinal?.produtos.map((produto) => ({
+                    orc_filial: produto.orc_filial,
+                    orc_numero: produto.orc_numero,
+                    orc_serie: produto.orc_serie,
+                    orc_valor_liquido: produto.orc_valor_liquido,
+                    orc_volume: produto.orc_volume,
+                    orc_Volume_Unidade: produto.orc_Volume_Unidade,
+                    orc_qt_potes: produto.orc_qt_potes,
+                    orc_forma_farmac: produto.orc_forma_farmac,
+                    orcamentoItens: produto.orcamentoItens.map((item) => ({
+                        orc_Produto_ID: item.orc_Produto_ID,
+                        orc_Produto_Nome: item.orc_Produto_Nome,
+                        orc_Produto_quantidade: item.orc_Produto_quantidade,
+                        orc_Produto_unidade: item.orc_Produto_unidade,
+                        orc_Produto_portaria: item.orc_Produto_portaria || null,
+                    })),
+                    orcamentoSugestoesPreco: produto.orcamentoSugestoesPreco || null,
+                })),
+                frete: checkout.orcamentoFinal?.frete,
+                total: checkout.orcamentoFinal?.total,
+            },
+        }));
 
-        return checkouts;
+        return formattedCheckouts;
     } catch (error) {
         throw error;
     }
 };
+
+
 
 module.exports = {
     processCheckout,
