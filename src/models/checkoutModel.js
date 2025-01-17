9bfbd195456a37969703ff60b7150149af7358d0
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-01-17 20:35:59 -0300

    Update checkoutModel.js

--- src/models/checkoutModel.js
+++ src/models/checkoutModel.js
@@ -1,6 +1,6 @@
-// models/Checkout.js
 const mongoose = require('mongoose');
 
+// Definição dos schemas de itens de orçamento, endereço e checkout
 const orcamentoItemSchema = new mongoose.Schema({
     orc_Produto_ID: { type: Number, required: true },
     orc_Produto_Nome: { type: String, required: true },
@@ -9,8 +9,19 @@ const orcamentoItemSchema = new mongoose.Schema({
     orc_Produto_portaria: { type: String, required: false }
 });
 
+const enderecoSchema = new mongoose.Schema({
+    bairro: { type: String, required: true },
+    cep: { type: String, required: true },
+    cidade: { type: String, required: true },
+    complemento: { type: String, required: false },
+    endereco: { type: String, required: true },
+    estado: { type: String, required: true },
+    numero: { type: String, required: true },
+    tipoFrete: { type: String, required: true }
+});
+
 const checkoutSchema = new mongoose.Schema({
-    orcamentoFinal: [{
+    orcamentoFinal: {
         checkout: { type: String, required: true },
         dataFim: { type: String, required: true },
         dadosPessoais: {
@@ -20,7 +31,7 @@ const checkoutSchema = new mongoose.Schema({
             celular: { type: String, required: true },
             email: { type: String, required: false }
         },
-        enderecoEntrega: { type: String, required: false },
+        enderecoEntrega: { type: enderecoSchema, required: false, default: null },
         localRetirada: { type: String, required: true },
         formaPagamento: { type: String, required: true },
         produtos: [{
@@ -37,9 +48,33 @@ const checkoutSchema = new mongoose.Schema({
         }],
         frete: { type: Number, required: true },
         total: { type: Number, required: true }
-    }],
+    },
     status: { type: String, required: true },
-    dataCompra: { type: Date, required: true }
+    dataCompra: { type: Date, required: true },
+    numero_orcamento: { type: String, required: true }
+});
+
+// Emitir eventos via WebSocket ao salvar, atualizar ou remover checkouts
+checkoutSchema.post('save', function(doc) {
+    const io = global.io; // Certifique-se de que `global.io` está configurado
+    if (io) {
+        io.emit('newCheckout', doc);
+    }
+});
+
+checkoutSchema.post('findOneAndUpdate', async function() {
+    const io = global.io;
+    if (io) {
+        const updatedDoc = await this.model.findById(this.getQuery()._id);
+        io.emit('checkoutUpdated', updatedDoc);
+    }
+});
+
+checkoutSchema.post('remove', function(doc) {
+    const io = global.io;
+    if (io) {
+        io.emit('checkoutRemoved', { message: 'Checkout removido', checkoutId: doc._id });
+    }
 });
 
 const Checkout = mongoose.model('Checkout', checkoutSchema, 'carrinho');
