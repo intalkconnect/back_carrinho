f4031acef6909372374e9887bd8f00057a1c374b
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-01-17 20:34:34 -0300

    Update checkoutController.js

--- src/controllers/checkoutController.js
+++ src/controllers/checkoutController.js
@@ -3,17 +3,23 @@ const checkoutService = require('../services/checkoutService');
 
 const finishCheckout = async (req, res) => {
     try {
-        const { checkout, dataFim, dadosPessoais, enderecoEntrega, localRetirada, formaPagamento, produtos, frete, total } = req.body;
-
-        const updatedCheckout = await checkoutService.processCheckout(
-            checkout, dataFim, dadosPessoais, enderecoEntrega, localRetirada, formaPagamento, produtos, frete, total
-        );
-
-        res.status(200).json({ message: 'Checkout processado com sucesso!' });
+      const { checkout, dataFim, dadosPessoais, enderecoEntrega, localRetirada, formaPagamento, produtos, frete, total, numero_orcamento } = req.body;
+  
+      // Se enderecoEntrega não for um objeto válido, definimos como null
+      if (enderecoEntrega && typeof enderecoEntrega !== 'object') {
+        return res.status(400).json({ error: 'enderecoEntrega deve ser um objeto ou null' });
+      }
+  
+      const updatedCheckout = await checkoutService.processCheckout(
+        checkout, dataFim, dadosPessoais, enderecoEntrega || null, localRetirada, formaPagamento, produtos, frete, total, numero_orcamento
+      );
+  
+      res.status(200).json({ message: 'Checkout processado com sucesso!' });
     } catch (error) {
-        res.status(500).json({ error: 'Erro ao processar o checkout.' });
+      res.status(500).json({ error: 'Erro ao processar o checkout.' });
     }
-};
+  };
+  
 
 const getCheckoutsController = async (req, res) => {
     try {
